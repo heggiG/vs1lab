49f3039bea84ad546bd174f57c2aad21e2cf1efa
HeggiG <florianheck0@gmail.com>
2022-06-12 23:21:13 +0200

    added the listeners on dom loaded

--- Aufgabe4/gta_v4_template/public/javascripts/formevaluator.js
+++ Aufgabe4/gta_v4_template/public/javascripts/formevaluator.js
@@ -1,33 +1,37 @@
 import GeoTag from "../../models/geotag";
 
+document.addEventListener('DOMContentLoaded', () => {
 
-document.getElementById("tag-form").addEventListener("submit", () => {
-    let name = document.getElementById('inp_name');
-    let tag = document.getElementById('inp_hashtag');
-    let longitude = document.getElementById('inp_longitude');
-    let latitude = document.getElementById('inp_latitude');
-    let geotag = new GeoTag(latitude, longitude, name, tag);
-    let id = -1;
-    fetch(`/api/geotags/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(geotag),
-    }).then(r => console.log(r)).catch(e => console.error(e))
+    document.getElementById("tag-form").addEventListener("submit", () => {
+        let name = document.getElementById('inp_name');
+        let tag = document.getElementById('inp_hashtag');
+        let longitude = document.getElementById('inp_longitude');
+        let latitude = document.getElementById('inp_latitude');
+        let geotag = new GeoTag(latitude, longitude, name, tag);
+        let id = -1;
+        fetch(`/api/geotags/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(geotag),
+        }).then(r => console.log(r)).catch(e => console.error(e))
 
-    //map erneuern
-});
+        //map erneuern
+    });
 
-document.getElementById("discoveryFilterForm").addEventListener("submit", () => {
-    let form = document.getElementById("discoveryFilterForm");
-    let id = -1;
-    fetch(`/api/geotags/${id}`)
-        .then(r => r.json())
-        .then(r => {}) //id finden
-        .catch(e => console.error(e));
+    document.getElementById("discoveryFilterForm").addEventListener("submit", () => {
+        let form = document.getElementById("discoveryFilterForm");
+        let id = -1;
+        fetch(`/api/geotags/${id}`)
+            .then(r => r.json())
+            .then(r => {
+            }) //id finden
+            .catch(e => console.error(e));
 
-    //map erneuern
-    //tag liste erneuern
+        //map erneuern
+        //tag liste erneuern
+    });
+
 });
 
