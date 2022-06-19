9e5518f6d402380bdfb9ed14d7dcf2a7944ec5bc
hagen <h.murki@t-online.de>
2022-06-19 18:59:20 +0200

    fixed size decreasing error

--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -264,7 +264,6 @@ router.delete('/api/geotags/:id', (req, res) => {
 
 router.get('/api/geotags/page/:id', (req, res) => {
   let tagStorage = GeoTagStore.getInstance();
-  
   let searchterm = req.query.searchterm
   let geotags = []
   if (searchterm == "undefined") { 
@@ -272,7 +271,6 @@ router.get('/api/geotags/page/:id', (req, res) => {
   } else {
     geotags = tagStorage.searchGeoTags(searchterm);
   }
-
   res.json(JSON.stringify(Paging.getPage(geotags, req.params.id-1)));
 });
 
